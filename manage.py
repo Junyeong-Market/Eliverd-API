6eb6bbe6d3e533b68088658a585f5821a955b64d
startergate <startergate36@gmail.com>
2020-04-24 21:57:54 +0900

    fix: typo in manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elivert.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eliverd.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
