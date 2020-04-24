7726d9dd00bf552607222a9f84ba2c0b3a0247f8
startergate <startergate36@gmail.com>
2020-04-24 21:57:40 +0900

    fix: typo

--- eliverd/urls.py
+++ eliverd/urls.py
@@ -1,4 +1,4 @@
-"""elivert URL Configuration
+"""eliverd URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
