d427378ad44d5fc8b7323cb3c0978c9cf7ddae2a
startergate <startergate36@gmail.com>
2020-05-01 17:08:59 +0900

    add: user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    def __str__(self):
+        return str(self.id)
+
+    pid = models.AutoField(primary_key=True)
+    id = models.CharField(unique=True)
+    password = models.CharField(max_length=64)
+    nickname = models.CharField(unique=True)
+    isSeller = models.BooleanField(null=False, default=False)
+
