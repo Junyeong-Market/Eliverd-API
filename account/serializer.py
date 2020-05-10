d64cdad313b97a6f97f70c335456767b718b4153
startergate <startergate36@gmail.com>
2020-05-10 21:14:35 +0900

    add: account serializer

---
+++ account/serializer.py
@@ -0,0 +1,8 @@
+from account.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        field = ['pid', 'id', 'password', 'nickname', 'isSeller']
