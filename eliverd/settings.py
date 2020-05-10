67c2fd8d6844547edc9ac410ca7fa122ee52f6dd
startergate <startergate36@gmail.com>
2020-05-10 21:14:05 +0900

    [UPDATE] using maria db

--- eliverd/settings.py
+++ eliverd/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'eliverd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mariadb',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
