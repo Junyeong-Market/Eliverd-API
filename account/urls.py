b4187f24be9a0a95d29b2ffd929a120bf04e39b6
startergate <startergate36@gmail.com>
2020-05-13 20:18:35 +0900

    add: user related router

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from account.views import UserInfoAPI, SessionAPI, create_user
+
+urlpatterns = [
+    path('session/', SessionAPI.as_view()),
+    path('user/', create_user),
+    path('user/<pid>/', UserInfoAPI.as_view())
+]
