e0eee789fee731a48b15f79e9a8f45cf7473ce39
startergate <startergate36@gmail.com>
2020-05-13 20:13:24 +0900

    add: user related view

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from account.permissions import NotLoggedIn, LoggedIn
 from account.serializer import UserSerializer
 
 
@@ -13,4 +15,22 @@ def create_user(request):
         return Response(new_user.data, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+class UserInfoAPI(APIView):
+    permission_classes = [LoggedIn]
+
+    def put(self, request, pid):
+        if request.account.pid != pid:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = UserSerializer(request.account, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+        return Response({'data': serializer.data})
+
+    def delete(self, request, pid):
+        if request.account.pid != pid:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response(status=status.HTTP_200_OK)
 
