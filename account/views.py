86cf4c0c37cc61cfa7ef2d8b5eea703811e58289
startergate <startergate36@gmail.com>
2020-05-13 19:25:22 +0900

    add: user register

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from account.serializer import UserSerializer
+
+
+@api_view(['POST'])
+def create_user(request):
+    new_user = UserSerializer(data=request.data)
+    if new_user.is_valid():
+        new_user.save()
+        return Response(new_user.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
