fbaaa102e3eab8bebf72f9c591b5e78908f2226a
startergate <startergate36@gmail.com>
2020-05-16 17:34:31 +0900

    add: Store model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.gis.db import models as geo_models
 
-# Create your models here.
+from account.models import User
+
+
+class Store(geo_models.Model):
+    def __str__(self):
+        return self.name
+
+    id = geo_models.AutoField(primary_key=True)
+    name = geo_models.CharField(max_length=50, null=False)
+    description = geo_models.TextField(null=False)
+    registerer = geo_models.ForeignKey(User, to_field=User.pid, on_delete=geo_models.CASCADE)
+    registered_number = geo_models.CharField(max_length=10)
+    location = geo_models.PointField(null=False)
+
+
