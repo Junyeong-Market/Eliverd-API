1cd86e87b1a1e40ac473d92a5660670c1d70ddb1
startergate <startergate36@gmail.com>
2020-05-17 18:08:40 +0900

    feat: move product model to product app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+
+
+class Product(models.Model):
+    def __str__(self):
+        return self.name
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, null=False)
+    manufacturer = models.ForeignKey(Manufacturer, models.CASCADE)
+    ian = models.CharField(max_length=17, null=True)  # International Article Number == 바코드
+
--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.gis.db import models as geo_models
 
 from account.models import User
+from product.models import Product
 
 
 class Store(geo_models.Model):
@@ -14,13 +15,4 @@ class Store(geo_models.Model):
     registerer = geo_models.ForeignKey(User, to_field=User.pid, on_delete=geo_models.CASCADE)
     registered_number = geo_models.CharField(max_length=10)
     location = geo_models.PointField(null=False)
-
-
-class Product(models.Model):
-    def __str__(self):
-        return self.name
-
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=128, null=False)
-    manufacturer = models.ForeignKey()
 
