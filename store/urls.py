c635250698d9d591c038b7910b2a19895f0d3631
startergate <startergate36@gmail.com>
2020-05-25 20:18:14 +0900

    feat: change url

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ router = routers.SimpleRouter()
 
 router.register(r'^by-radius/$', RadiusStoreView)
 # router.register(r'^by-area/{area}/$', AreaStoreView)
-router.register(r'^(?P<id>.+)', StoreStockView)
+router.register(r'^(?P<id>.+)/stocks$', StoreStockView)
 
 urlpatterns = router.urls
