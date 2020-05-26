4a92ea5dae5a7ecc60456f7b2b1da737de5fa9e1
startergate <startergate36@gmail.com>
2020-05-26 17:09:35 +0900

    fix: override authentication classes

--- eliverd/settings.py
+++ eliverd/settings.py
@@ -113,6 +113,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+    #    'rest_framework.authentication.BasicAuthentication',
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
