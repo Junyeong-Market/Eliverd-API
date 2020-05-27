7423b69051d887466b492e0af2ade1e2e28a8e1a
startergate <startergate36@gmail.com>
2020-05-27 16:46:07 +0900

    feat: change api view's name

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,16 @@
+from django.urls import path
 from rest_framework import routers
 
-from store.views import RadiusStoreView, StoreStockView
+from store.views import RadiusStoreList, StoreStockList, StoreView
 
 router = routers.SimpleRouter()
 
-router.register(r'^by-radius/$', RadiusStoreView)
+router.register(r'^by-radius/$', RadiusStoreList)
 # router.register(r'^by-area/{area}/$', AreaStoreView)
-router.register(r'^(?P<id>.+)/stocks$', StoreStockView)
+router.register(r'^(?P<id>.+)/stocks$', StoreStockList)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('by-radius/', RadiusStoreList.as_view()),
+    path('{id}/', StoreView.as_view()),
+    path('{id}/stocks', StoreStockList.as_view())
+]
--- store/views.py
+++ store/views.py
@@ -1,15 +1,16 @@
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from store.models import Store
 from store.serializer import StoreSerializer
 
 
-class RadiusStoreView(ListAPIView):
+class RadiusStoreList(ListAPIView):
     queryset = Store.objects.filter()
     serializer_class = StoreSerializer
 
@@ -17,6 +18,10 @@ class RadiusStoreView(ListAPIView):
         point = Point(self.request.query_params.get('lat'), self.request.query_params.get('lng'))
         distance = D(m=self.request.query_params.distance)
         return Store.objects.filter(location__distance_lte=(point, distance))
+
+    @classmethod
+    def get_extra_actions(cls):
+        return []
 
 
 # class AreaStoreView(ListAPIView):
@@ -26,10 +31,16 @@ class RadiusStoreView(ListAPIView):
 #     def get_queryset(self):
 #         return Store.objects.filter()
 
+class StoreView(APIView):
 
-class StoreStockView(ListAPIView):
+    def get(self):
+        return Response(Store.objects.get(id=self.kwargs['id']))
+
+
+class StoreStockList(RetrieveAPIView):
     queryset = Store.objects.filter()
     serializer_class = StoreSerializer
+    lookup_field = 'products'
 
     def get_queryset(self):
         return Store.objects.filter(id=self.kwargs['id'])
