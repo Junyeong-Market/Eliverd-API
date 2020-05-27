5e1adc8ed9f0f7212fdabb0657586c8a7a8683aa
startergate <startergate36@gmail.com>
2020-05-27 16:44:38 +0900

    fix: model not working

--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ from django.db import models
 
 class User(models.Model):
     def __str__(self):
-        return str(self.id)
+        return str(self.user_id)
 
     pid = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=50, unique=True)
@@ -19,8 +19,8 @@ class User(models.Model):
 
 class Session(models.Model):
 
-    def __init__(self):
-        super(Session, self).__init__()
+    def __init__(self, *args, **kwargs):
+        super(Session, self).__init__(*args, **kwargs)
         expire = datetime.datetime.now()
         expire.replace(day=expire.day+3)
         self.expireAt = expire
