c3bfa07f73498e8286da6a08a57a302dd263965b
startergate <startergate36@gmail.com>
2020-05-27 16:45:37 +0900

    feat: update permissions using User classes

--- account/permissions.py
+++ account/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework.permissions import BasePermission
 
+from account.user import AnonymousUser, NormalUser
+
 
 class NotLoggedIn(BasePermission):
     def has_permission(self, request, view):
-        return not request.user
+        return request.user is AnonymousUser
 
 
 class LoggedIn(BasePermission):
     def has_permission(self, request, view):
-        return request.user
+        return request.user is NormalUser
