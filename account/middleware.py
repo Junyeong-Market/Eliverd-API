97bfdf9c3656981c1618ddc0418348caf427d6de
startergate <startergate36@gmail.com>
2020-05-27 17:09:06 +0900

    feat: request.account 사용

--- account/middleware.py
+++ account/middleware.py
@@ -1,4 +1,10 @@
-from account.models import User, Session
+import logging
+from datetime import datetime
+
+from account.models import Session
+from account.user import AnonymousUser, NormalUser
+
+logger = logging.getLogger(__name__)
 
 
 class AuthorizationMiddleware:
@@ -6,9 +12,12 @@ class AuthorizationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        pid = Session.objects.filter(id=request.headers.get('Authorization'))
+        try:
+            session = Session.objects.get(id=request.headers.get('Authorization', -1), expireAt__gte=datetime.now())
+            request.account = NormalUser(session.pid)
+        except Session.DoesNotExist:
 
-        request.user = User.objects.filter(pid=pid)
+            request.account = AnonymousUser
 
         response = self.get_response(request)
 
