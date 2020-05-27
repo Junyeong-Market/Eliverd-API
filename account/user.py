983ba9cc590b65279ea46a34446b7ad7232180df
startergate <startergate36@gmail.com>
2020-05-27 16:45:10 +0900

    add: mocking django user

--- account/user.py
+++ account/user.py
@@ -1,6 +1,20 @@
 class AbstractUser:
     pid: int
-    user_id: string
-    password = models.CharField(max_length=64)
-    nickname:
-    isSeller: bool
+    user_id: str
+    nickname: str
+    is_seller: bool
+
+
+class AnonymousUser(AbstractUser):
+    pid = -1
+    user_id = ""
+    nickname = ""
+    isSeller = ""
+
+
+class NormalUser(AbstractUser):
+    def __init__(self, model):
+        self.pid = model.pid
+        self.user_id = model.user_id
+        self.nickname = model.nickname
+        self.is_seller = model.is_seller
