80f56d6dcd5ec861d749eccdbbc003b2576d518f
startergate <startergate36@gmail.com>
2020-06-15 20:20:22 +0900

    add: create store api to url

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from store.views import RadiusStoreList, StoreStockListAPI, StoreView, AddStockAPI, RemoveStockAPI
+from store.views import RadiusStoreList, StoreStockListAPI, StoreView, AddStockAPI, RemoveStockAPI, CreateStoreAPI
 
 urlpatterns = [
+    path('', CreateStoreAPI.as_view()),
     path('by-radius/', RadiusStoreList.as_view()),
     path('<id>/', StoreView.as_view()),
     path('<id>/stocks', StoreStockListAPI.as_view()),
