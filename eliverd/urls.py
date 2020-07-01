d2ccbcbaf7dcadf769aafb81448f7ad5faa254da
startergate <startergate36@gmail.com>
2020-07-01 10:11:34 +0900

    add: product app 접근 가능

--- eliverd/urls.py
+++ eliverd/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('account/', include('account.urls')),
+    path('product/', include('product.urls')),
     path('store/', include('store.urls'))
 ]
