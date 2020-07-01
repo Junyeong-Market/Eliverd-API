962c62fdbdeb4adc12a1405a61ad164a5a1f1698
startergate <startergate36@gmail.com>
2020-07-01 09:37:52 +0900

    fix: url slash 통일

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', CreateStoreAPI.as_view()),
     path('by-radius/', RadiusStoreList.as_view()),
     path('<id>/', StoreView.as_view()),
-    path('<id>/stocks', StoreStockListAPI.as_view()),
-    path('<id>/stock', ModifyStockAPI.as_view())
+    path('<id>/stocks/', StoreStockListAPI.as_view()),
+    path('<id>/stock/', ModifyStockAPI.as_view())
 ]
