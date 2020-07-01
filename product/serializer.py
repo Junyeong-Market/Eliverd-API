b933fd69e620825d69dfa2033c7fc8ca86c6f757
startergate <startergate36@gmail.com>
2020-07-01 10:12:06 +0900

    add: manufacturer serializer

--- product/serializer.py
+++ product/serializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from product.models import Product
+from product.models import Product, Manufacturer
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
         fields = '__all__'
