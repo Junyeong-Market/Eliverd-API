fca18c2d066dd65873cf255c99ec04f1e0748e13
startergate <startergate36@gmail.com>
2020-07-01 10:27:23 +0900

    docs: 상품 관련 API 문서화

--- product/views.py
+++ product/views.py
@@ -10,6 +10,11 @@ class GetProductAPI(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    @swagger_auto_schema(operation_summary='상품 정보 조회',
+                         operation_description='상품 정보를 가져옵니다.')
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
     def get_object(self):
         return get_object_or_404(Product, ian=self.kwargs['ian'])
 
@@ -24,7 +29,7 @@ class SearchManufacturerAPI(ListAPIView):
     serializer_class = ManufacturerSerializer
     pagination_class = ManufacturerSearchPagination
 
-    @swagger_auto_schema(operation_summary='제조사 검색 API',
+    @swagger_auto_schema(operation_summary='제조사 검색',
                          operation_description='지정한 문자열이 포함된 이름을 가진 제조사를 검색합니다.')
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
