c4349fce03e2bb291e47d88bf9c5f0133609712d
startergate <startergate36@gmail.com>
2020-07-06 14:06:19 +0900

    add: purchase 기본 구조
    closes #18

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+urlpatterns = [
+    # TODO: path('/'), order 생성
+    # TODO: path('<oid>/'), order 조회
+    # TODO: path('<oid>/success/'), order 성공 (kp 전용)
+    # TODO: path('<oid>/fail/'), order 실패 (kp 전용)
+    # TODO: path('<oid>/cancel/'), order 취소 (kp 전용)
+]
