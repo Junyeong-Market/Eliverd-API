5768bafb26d44db912a556c44b389e3ce7a36518
startergate <startergate36@gmail.com>
2020-07-06 17:34:19 +0900

    add: prototype of create order api

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from purchase.views import CreateOrderAPI
+
 urlpatterns = [
-    # TODO: path('/'), order 생성
+    path('/', CreateOrderAPI),
     # TODO: path('<oid>/'), order 조회
-    # TODO: path('<oid>/success/'), order 성공 (kp 전용)
+    # TODO: path('<oid>/approve/'), order 성공 (kp 전용)
     # TODO: path('<oid>/fail/'), order 실패 (kp 전용)
     # TODO: path('<oid>/cancel/'), order 취소 (kp 전용)
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+import requests
+
+from rest_framework.generics import CreateAPIView
+
+
+class CreateOrderAPI(CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        res = super().post(request, *args, **kwargs)
+        oid = res.data['oid']
+        requests.post('https://kapi.kakao.com/v1/payment/ready',
+                      data={
+                          'cid': os.getenv('KAKAOPAY_CID'),
+                          'partner_order_id': oid,
+                          'partner_user_id': request.account.pid,
+                          'item_name': 'some name',
+                          'quantity': 1,
+                          'total_amount': 0,
+                          'vat_amount': 'total_amount / 11',
+                          'tax_free_amount': 0,
+                          'approval_url': f"/purchase/{oid}/success/",
+                          'fail_url': f"/purchase/{oid}/fail/",
+                          'cancel_url': f"/purchase/{oid}/cancel/"
+                      },
+                      headers={
+                          'Authorization': f"KakaoAK {os.getenv('KAKAO_ADMIN')}"
+                      })
+
