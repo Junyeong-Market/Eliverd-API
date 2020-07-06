f985e8df9258400d7bcf937966e5308d780276d3
startergate <startergate36@gmail.com>
2020-07-06 17:33:46 +0900

    add: order model

--- purchase/models.py
+++ purchase/models.py
@@ -4,6 +4,13 @@ from django.db import models
 from store.models import Stock
 
 
+class OrderStatus(models.TextChoices):
+    PENDING = "pending"
+    PROCESSED = "processed"
+    CANCELED = "canceled"
+    FAILED = "failed"
+
+
 class OrderedStock(models.Model):
     def __str__(self):
         return self.stock.product.name + ' x ' + str(self.amount)
@@ -11,3 +18,9 @@ class OrderedStock(models.Model):
     osid = models.AutoField(primary_key=True)
     stock = models.ForeignKey(Stock, models.CASCADE)
     amount = models.PositiveIntegerField()
+
+
+class Order(models.Model):
+    oid = models.AutoField(primary_key=True)
+    tid = models.CharField(max_length=20, unique=True)
+    stocks = models.ManyToManyField(OrderedStock)
