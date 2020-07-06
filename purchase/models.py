2c81791af24ce70d3f93094006764ceea15b9005
startergate <startergate36@gmail.com>
2020-07-06 17:21:46 +0900

    add: ordered stock model

--- purchase/models.py
+++ purchase/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from store.models import Stock
+
+
+class OrderedStock(models.Model):
+    def __str__(self):
+        return self.stock.product.name + ' x ' + str(self.amount)
+
+    osid = models.AutoField(primary_key=True)
+    stock = models.ForeignKey(Stock, models.CASCADE)
+    amount = models.PositiveIntegerField()
