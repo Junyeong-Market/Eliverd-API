b0d23ae5e5ec5e64fe45b0b9c4c35834f743651f
startergate <startergate36@gmail.com>
2020-07-07 15:29:24 +0900

    fix: permissions not working

--- account/permissions.py
+++ account/permissions.py
@@ -1,13 +1,17 @@
+import logging
+
 from rest_framework.permissions import BasePermission
 
 from account.user import AnonymousUser, NormalUser
 
+logger = logging.getLogger(__name__)
+
 
 class NotLoggedIn(BasePermission):
     def has_permission(self, request, view):
-        return request.account is AnonymousUser
+        return isinstance(request.account, AnonymousUser)
 
 
 class LoggedIn(BasePermission):
     def has_permission(self, request, view):
-        return request.account is NormalUser
+        return isinstance(request.account, NormalUser)
