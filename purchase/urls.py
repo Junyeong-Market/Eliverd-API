51f813c68327617f35ae15e6dd4f43ac6fdb1d60
startergate <startergate36@gmail.com>
2020-07-21 16:11:26 +0900

    fix: missing as_view

--- purchase/urls.py
+++ purchase/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from purchase.views import CreateOrderAPI
 
 urlpatterns = [
-    path('/', CreateOrderAPI),
+    path('/', CreateOrderAPI.as_view()),
     # TODO: path('<oid>/'), order 조회
     # TODO: path('<oid>/approve/'), order 성공 (kp 전용)
     # TODO: path('<oid>/fail/'), order 실패 (kp 전용)
