754efd465879f1d4295a620dcf00a335202d7f0d
startergate <startergate36@gmail.com>
2020-08-05 11:13:11 +0900

    fix: routing problem

--- purchase/urls.py
+++ purchase/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from purchase.views import CreateOrderAPI, FailedOrderAPI, CancelOrderAPI, GetOrderAPI
 
 urlpatterns = [
-    path('/', CreateOrderAPI.as_view()),
+    path('', CreateOrderAPI.as_view()),
     path('<oid>/', GetOrderAPI.as_view()),  # order 조회
     # TODO: path('<oid>/approve/'), order 성공 (kp 전용)
     path('<oid>/fail/', FailedOrderAPI.as_view()),  # order 실패 (kp 전용)
