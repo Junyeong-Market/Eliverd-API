f04fa7b48c2a3efca0f492453fd9854d604b6d77
startergate <startergate36@gmail.com>
2020-08-24 14:18:13 +0900

    fix: 비밀번호 표시 제거

--- purchase/serializer.py
+++ purchase/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+from account.serializer import SafeUserSerializer
 from purchase.models import OrderedStock, Order, PartialOrder
+from store.serializer import StoreSerializer, GetStockSerializer
 
 
 class OrderedStockSerializer(serializers.ModelSerializer):
@@ -22,6 +24,33 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class DeepOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        depth = 5
+        fields = '__all__'
+
+
+class GetOrderedStockSerializer(serializers.ModelSerializer):
+    stock = GetStockSerializer()
+
+    class Meta:
+        model = OrderedStock
+        fields = '__all__'
+
+
+class GetPartialOrderSerializer(serializers.ModelSerializer):
+    store = StoreSerializer()
+    stocks = GetOrderedStockSerializer(many=True)
+
+    class Meta:
+        model = PartialOrder
+        fields = '__all__'
+
+
+class GetOrderSerializer(serializers.ModelSerializer):
+    customer = SafeUserSerializer()
+    partials = GetPartialOrderSerializer(many=True)
+
     class Meta:
         model = Order
         depth = 5
