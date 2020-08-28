ba431518b4d64033222ea340fa134710ca94c95c
startergate <startergate36@gmail.com>
2020-08-28 10:10:49 +0900

    feat: 범위 기반 상품 검색

--- product/urls.py
+++ product/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from product.views import GetProductAPI, SearchManufacturerAPI, CreateManufacturerAPI, UpdateManufacturerAPI, \
-    RecommendedProductListAPI
+    RecommendedProductListAPI, RadiusProductListAPI
 
 urlpatterns = [
     path('manufacturer/', CreateManufacturerAPI.as_view()),
     path('manufacturer/<id>/', UpdateManufacturerAPI.as_view()),
     path('manufacturer/search/<name>/', SearchManufacturerAPI.as_view()),
+    path('by-radius/', RadiusProductListAPI.as_view()),
     path('recommended/', RecommendedProductListAPI.as_view()),
     path('<ian>/', GetProductAPI.as_view()),
 ]
