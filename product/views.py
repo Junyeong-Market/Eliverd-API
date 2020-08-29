2125909e019ec030cb39fe32eff91d9edccf6648
startergate <startergate36@gmail.com>
2020-08-29 23:53:24 +0900

    fix: 품절된 상품은 표시하지 않음

--- product/views.py
+++ product/views.py
@@ -11,7 +11,7 @@ from product.pagination import ManufacturerSearchPagination
 from product.serializer import ProductSerializer, ManufacturerSerializer
 from store.documentation import Lat, Lng, Distance, ProductName, Categories
 from store.models import Stock
-from store.serializer import StockSerializer
+from store.serializer import StockSerializer, GetStockSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -92,7 +92,7 @@ class RadiusProductListAPI(ListAPIView):
 
 
 class RecommendedProductListAPI(ListAPIView):
-    serializer_class = StockSerializer
+    serializer_class = GetStockSerializer
 
     @swagger_auto_schema(operation_summary='오늘의 상품 추천',
                          operation_description='지정된 범위 내의 상품을 가져옵니다.',
@@ -103,4 +103,4 @@ class RecommendedProductListAPI(ListAPIView):
     def get_queryset(self):
         point = Point(float(self.request.query_params.get('lat')), float(self.request.query_params.get('lng')))
 
-        return Stock.objects.order_by('?').filter(store__location__distance_lte=(point, 5))[:5]
+        return Stock.objects.order_by('?').filter(store__location__distance_lte=(point, 5), amount__gt=0)[:5]
