c58f2c90d497f3274ef461f265aefa530035d9d3
startergate <startergate36@gmail.com>
2020-09-08 17:12:08 +0900

    fix: user class has is_seller

--- account/user.py
+++ account/user.py
@@ -6,14 +6,12 @@ class AbstractUser:
     pid: int
     user_id: str
     nickname: str
-    is_seller: bool
 
 
 class AnonymousUser(AbstractUser):
     pid = -1
     user_id = ""
     nickname = ""
-    isSeller = ""
 
 
 class NormalUser(AbstractUser):
@@ -22,4 +20,3 @@ class NormalUser(AbstractUser):
         self.pid = model.pid
         self.user_id = model.user_id
         self.nickname = model.nickname
-        self.is_seller = model.is_seller
