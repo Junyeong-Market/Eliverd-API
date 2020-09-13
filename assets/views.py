9b3ee619610fda593f96374aa1460ef698daa0e5
startergate <startergate36@gmail.com>
2020-09-14 00:29:44 +0900

    docs: 조금 더 자세한 설명

--- assets/views.py
+++ assets/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
 from rest_framework.generics import CreateAPIView
 from rest_framework.parsers import FileUploadParser
 
@@ -11,7 +12,10 @@ class FileUploadAPI(CreateAPIView):
     serializer_class = AssetSerializer
     parser_classes = [FileUploadParser]
 
-    @swagger_auto_schema(operation_summary='파일 업로드', operation_description='파일을 AWS S3로 업로드합니다.')
+    @swagger_auto_schema(operation_summary='파일 업로드',
+                         operation_description='파일을 AWS S3로 업로드합니다. Content-Type는 파일 MIME을 사용합니다. '
+                                               'Content-Disposition 헤더로 `Content-Disposition:inline;filename=(파일 '
+                                               '이름)`을 포함합니다.')
     def post(self, request, *args, **kwargs):
         request.data['image'] = request.data['file']
         return super().post(request, *args, **kwargs)
