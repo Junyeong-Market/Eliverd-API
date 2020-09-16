21ae12dd7a2728054d8f927cc34f74d8dd4c19c0
startergate <startergate36@gmail.com>
2020-09-16 09:37:06 +0900

    feat: 파일 조회

--- assets/urls.py
+++ assets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from assets.views import FileUploadAPI
+from assets.views import FileUploadAPI, GetAssetAPI
 
 urlpatterns = [
-    path('', FileUploadAPI.as_view())
+    path('', FileUploadAPI.as_view()),
+    path('<id>/', GetAssetAPI.as_view())
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,8 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.parsers import FileUploadParser
 
+from assets.models import Asset
 from assets.serializers import AssetSerializer
 
 
@@ -16,3 +17,14 @@ class FileUploadAPI(CreateAPIView):
     def post(self, request, *args, **kwargs):
         request.data['image'] = request.data['file']
         return super().post(request, *args, **kwargs)
+
+
+class GetAssetAPI(RetrieveAPIView):
+    serializer_class = AssetSerializer
+
+    @swagger_auto_schema(operation_summary='파일 조회', operation_description='파일을 조회합니다.')
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self):
+        return Asset.objects.get(id=self.kwargs['id'])
