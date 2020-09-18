321c0fb61a76f29ecae2c0d0496b617c30ef0477
startergate <startergate36@gmail.com>
2020-09-18 17:26:18 +0900

    refac: password 해시화 변경

--- account/models.py
+++ account/models.py
@@ -11,17 +11,20 @@ logger = logging.getLogger(__name__)
 
 
 class User(models.Model):
-    def __init__(self, *args, **kwargs):
-        super(User, self).__init__(*args, **kwargs)
-        self.password = hashlib.sha512(str(kwargs['password']).encode('utf-8')).hexdigest()\
-            if kwargs.get('password') else self.password
-
     def __str__(self):
         return str(self.user_id)
 
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = hashlib.sha512(str(value).encode('utf-8')).hexdigest()
+
     pid = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=256)
+    _password = models.CharField(max_length=256, name='password')
     realname = models.CharField(max_length=128, default='사용자')
     nickname = models.CharField(max_length=50, unique=True)
     home = models.PointField(null=True)
